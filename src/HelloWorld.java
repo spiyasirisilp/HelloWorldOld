8893ab50a8d218b1823f39535f4b0510e31964e9
spiyasirisilp <spiyasirisilp@gmail.com>
2016-03-03 20:18:34 -0500

    Deleted Hello World and Added Good Bye

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,6 @@
 
 public class HelloWorld {
 	public static void main(String [] args){
-		System.out.println("Hello Asians!!!");
-		
-		System.out.println("Hello Family!");
-
-		System.out.println("Hello fantasia!!!");
+		System.out.println("Goodbye world!!");
 	}
 }
